3cc81700a074fe65845c171acd02806cfd243601
christian telemaque <salvator8@hotmail.com>
2014-04-05 00:47:31 -0400

    ex6

--- tp_iINF2005/js/authentification.js
+++ tp_iINF2005/js/authentification.js
@@ -13,23 +13,23 @@ function valideInscription() {
 }
 
 function doublons() {
-	
+
 	var doublons = contain(email, liste1);
 
 }
 
 function contain(mail, chemin) {
-	
+
 	var resultat = true;
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.open("GET", chemin, true);
-	
+
 	xmlhttp.onreadystatechange = function() {
 
 		if (xmlhttp.readyState == 4) {
 			var xml = xmlhttp.responseXML;
 			var docs = xml.getElementsByTagName("email");
-		
+
 			for ( i = 0; i < docs.length; i++) {
 				if (mail == docs[i].firstChild.nodeValue) {
 					resultat = false;
@@ -37,7 +37,7 @@ function contain(mail, chemin) {
 				}
 
 			}
-			
+
 			if (resultat) {
 				save(mail);
 			}
@@ -55,39 +55,71 @@ function messageErreur() {
 }
 
 function save(mail) {
-	
-	
+
 	if (mail != confirmation) {
 		alert("The mail must be the same!");
 	} else {
 		alert("Inscription a été faite");
-		
-		localStorage.setItem("0",email);
-		localStorage.setItem("1",mpd);
-		localStorage.setItem("2","etudiant");
 
-		
-		
+		localStorage.setItem("0", email);
+		localStorage.setItem("1", mpd);
+		localStorage.setItem("2", "etudiant");
+
 	}
 
 }
 
-function accueil(){
+function accueil() {
 	var user = document.connection.email.value;
 	var pwd = document.connection.pwd.value;
 	var storedValue = localStorage.getItem("email");
-	
-	
-	for(i =0 ; i < localStorage.length; i++){
-		if((user == localStorage.getItem(""+i+""))&&(pwd == localStorage.getItem(""+(i+1)+""))){
-			localStorage.setItem("logIn",true);
-			localStorage.setItem("logOUT",false);
-			window.location.href = "Accueil.html";
-			
+
+	if (localStorage.getItem("block") == "true")
+		alert("** Your next login in "+ (new Date().getMinutes() - localStorage.getItem("nextLog") )+" minute");
+	else {
+		for ( i = 0; i < localStorage.length; i++) {
+			if ((user == localStorage.getItem("" + i + "")) && (pwd == localStorage.getItem("" + (i + 1) + ""))) {
+				localStorage.setItem("logIn", true);
+				localStorage.setItem("logOUT", false);
+				window.location.href = "Accueil.html";
+
+			}
 		}
+		
+		if (true) {
+
+		alert("Code d'accès ou Mot de passe incorrect");
+
+		if (localStorage.getItem("compteur") == 0)
+			localStorage.setItem("compteur", 1);
+		else if (localStorage.getItem("compteur") == 1) {
+			localStorage.setItem("compteur", 2);
+			if (localStorage.getItem("blockMinute2") > new Date().getMinutes()) {
+				localStorage.setItem("block", true);
+				localStorage.setItem("nextLog", (new Date().getMinutes() + 10));
+			} else {
+				init();
+			}
+
+		} else {
+			init();
+		}
+		alert(localStorage.getItem("compteur"));
+		alert(localStorage.getItem("block"));
+		
 	}
+		
+	}
+
 	
-	if(true)alert("Code d'accès ou Mot de passe incorrect");
-	
+
+}
+
+function init() {
+	localStorage.setItem("compteur", 0);
+	localStorage.setItem("block", false);
+	var m = new Date().getMinutes();
+	localStorage.setItem("blockMinute1", m);
+	localStorage.setItem("blockMinute2", m + 5);
 }
 
